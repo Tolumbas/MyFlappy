02d5a1ee1eb66e45465b51812bb9bfcab0e960ac
Feiro <tzat.anton@gmail.com>
2016-03-10 12:18:51 +0200

    made bird.h

--- Bird.cpp
+++ Bird.cpp
@@ -1,9 +1,23 @@
 #include "Bird.h"
-
+#include <math.h>
 
 
 Bird::Bird()
 {
+	vx = 0;
+	vy = 0;
+	setPosition(10,10);
+}
+
+void Bird::jump()
+{
+	vy = 5;
+}
+
+void Bird::update()
+{
+	move(vx, vy);
+	setRotation(atan2(vy, vx));
 }
 
 
--- Bird.h
+++ Bird.h
@@ -1,22 +1,14 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-class Bird : public sf::Drawable
+class Bird : public sf::Sprite
 {
+private:
+	float vx, vy;
+	float gravity;
 public:
 	Bird();
-private:
-	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
-	{
-		// You can draw other high-level objects
-		target.draw(m_sprite, states);
-		// ... or use the low-level API
-		states.texture = &m_texture;
-		target.draw(m_vertices, states);
-		// ... or draw with OpenGL directly
-		glBegin(GL_QUADS);
-		...
-			glEnd();
-	}
+	void jump();
+	void update();
 	sf::Sprite m_sprite;
 	sf::Texture m_texture;
 	sf::VertexArray m_vertices;
