e6a7abc0d88e871376d06c11f78bf32efeb7cf75
Feiro <tzat.anton@gmail.com>
2016-03-10 12:23:36 +0200

    some tests

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <SFML/Graphics.hpp>
+#include "bird.h";
 
 void main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
-	Bird* bird = new Bird();
+	Bird bird;
 	while (window.isOpen())
 	{
-		
+
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// Close window: exit
+			if (event.type == sf::Event::Closed)
+				window.close();
+			if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
+				bird.jump();
+		}
+
+		window.draw(bird);
+		bird.update();
 	}
 }
